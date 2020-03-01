4214c98e0ade6b07e66dd4b49bad44cd096b837c
local <local>
2020-03-01 14:55:52 -0800

    Finished setup for solving problems in java

---
+++ src/main/java/codeforces/CF1303C.java
@@ -0,0 +1,59 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Problem CF1303C
+ */
+public class CF1303C {
+
+    static class Task {
+        public void run(InputReader in, PrintWriter out) {
+
+        }
+    }
+
+    /***********************************************************
+     *                      BOILERPLATE                        *
+     ***********************************************************/
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        new Task().run(in, out);
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
