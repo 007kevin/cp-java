d50b70d3d5cedbde8298ce5e30e9720cc2ca94af
local <local@thinkpad.com>
2021-07-11 14:41:58 -0700

    Init atcoder repo

--- src/Template.java
+++ src/Template.java
@@ -4,129 +4,37 @@ import java.util.*;
 /**
  * Problem Template
  */
-@SuppressWarnings("unchecked")
 public class Template {
 
-    static class Task extends IOHandler {
-        public void run() {
-            
-        }
-    }
+    public void run() {
 
-    /***********************************************************
-     *                        COMMONS                          *
-     ***********************************************************/
-    static class Pair<A, B> implements Comparable<Pair<A, B>> {
-        public A a;
-        public B b;
-        public Pair(Pair<A, B> p) {
-            this(p.a, p.b);
-        }
-
-        public Pair(A a, B b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        public String toString() {
-            return a+" "+b;
-        }
-
-        public int hashCode() {
-            return Objects.hash(a, b);
-        }
-
-        public boolean equals(Object o) {
-            if(o instanceof Pair) {
-                Pair<A,B> p = (Pair<A,B>) o;
-                return a.equals(p.a)&&b.equals(p.b);
-            }
-            return false;
-        }
-
-        @Override
-        public int compareTo(Pair<A, B> p) {
-            int cmp = ((Comparable<A>) a).compareTo(p.a);
-            if(cmp==0) {
-                return ((Comparable<B>) b).compareTo(p.b);
-            }
-            return cmp;
-        }
     }
 
     /***********************************************************
      *                      BOILERPLATE                        *
-     ***********************************************************/
-    public static void main(String[] args) {
-        Task task = new Task();
-        task.run();
-        task.cleanup();
-    }
-
-    static class IOHandler {
-        public InputReader in = new InputReader(System.in);
-        public OutputWriter out = new OutputWriter(System.out);
-
-        public void cleanup() {
-            out.close();
-        }
-    }
-
+    /***********************************************************/
+    public InputReader in = new InputReader(System.in);
+    public PrintWriter out = new PrintWriter(System.out);
+    public void close() {out.close();}
     static class InputReader {
         public BufferedReader reader;
-        public StringTokenizer tokenizer;
-
+        public StringTokenizer tokens = null;
         public InputReader(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream), 32768);
-            tokenizer = null;
-        }
-
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);}
+        private boolean prime() {
+            while (tokens == null || !tokens.hasMoreTokens()) {
                 try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return tokenizer.nextToken();
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public int[] nextInts(int n) {
-            int a[] = new int[n];
-            for(int i = 0; i < n; ++i)
-                a[i] = nextInt();
-            return a;
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        public long[] nextLongs(int n) {
-            long a[] = new long[n];
-            for(int i = 0; i < n; ++i)
-                a[i] = nextLong();
-            return a;
-        }
+                    String line = reader.readLine();
+                    if(line==null) return false; // EOF
+                    tokens = new StringTokenizer(line);
+                } catch (IOException e) {throw new RuntimeException(e);}
+            } return true;}
+        public boolean hasNext() {return prime();}
+        public String next() {prime(); return tokens.nextToken();}
+        public int nextInt() {return Integer.parseInt(next());}
+        public long nextLong() {return Long.parseLong(next());}
+        public double nextDouble() {return Double.parseDouble(next());}
     }
-
-    static class OutputWriter extends PrintWriter {
-        public OutputWriter(OutputStream out) {
-            super(out);
-        }
-
-        public void println(String format, Object... args) {
-            this.println(String.format(format, args));
-        }
-
-        public void print(String format, Object... args) {
-            this.print(String.format(format, args));
-        }
-    }
-
+    public static void main(String[] args) {
+        Template task = new Template(); task.run(); task.close();}
 }
