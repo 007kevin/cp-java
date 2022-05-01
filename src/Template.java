0f93c622bee6c8ad4a75ab54212fe54c50f59783
Kevin <kevin@example.com>
2022-04-30 22:23:35 -0700

    better template formatting

--- src/Template.java
+++ src/Template.java
@@ -16,14 +16,14 @@ public class Template {
 
     }
 
-    IR in=new IR(System.in);PrintWriter out=new PrintWriter(System.out);void c(){
-    out.close();}static class IR {BufferedReader r; StringTokenizer t= null; IR (
-    InputStream s){r=new BufferedReader(new InputStreamReader(s), 32768);}boolean
-    p(){while(t==null||!t.hasMoreTokens()){try{String l=r. readLine();if(l==null)
-    return false; t = new StringTokenizer(l); } catch (IOException e) { throw new
-    RuntimeException(e);}}return true;}boolean hasNext(){return p();}String next(
-    ){ p();return t.nextToken();}int nextInt() {return Integer.parseInt(next());}
-    long nextLong() { return Long.parseLong(next());} double nextDouble(){ return
-    Double.parseDouble(next()); }} public static void main(String[]args){Template
-    t=new Template();t.run();t.c();}
-}
+/****************************************************************************/
+IR in=new IR(System.in);PrintWriter out=new PrintWriter(System.out);void c(){
+out.close();}static class IR {BufferedReader r; StringTokenizer t= null; IR (
+InputStream s){r=new BufferedReader(new InputStreamReader(s), 32768);}boolean
+p(){while(t==null||!t.hasMoreTokens()){try{String l=r. readLine();if(l==null)
+return false; t = new StringTokenizer(l); } catch (IOException e) { throw new
+RuntimeException(e);}}return true;}boolean hasNext(){return p();}String next(
+){ p();return t.nextToken();}int nextInt() {return Integer.parseInt(next());}
+long nextLong() { return Long.parseLong(next());} double nextDouble(){ return
+Double.parseDouble( next() ); } } public static void main ( String[ ]args ) {
+Template t=new Template();t.run();t.c();}}
